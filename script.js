3f6311f393969e170f42165d42e1dca5405315e2
theuspedrosa <matpedrosa@hotmail.com>
2025-10-06 12:11:30 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+}
+
+function toggleSobre() {
+  const sobre = document.getElementById("sobre")
+  sobre.classList.toggle("hidden")
+}
